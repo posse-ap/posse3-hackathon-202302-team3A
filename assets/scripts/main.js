f57524c4598e6dcdb3d2e01f8caa863645cf7610
koyo8 <iida_koyo@yahoo.co.jp>
2023-02-24 01:20:34 +0900

    モーダル部分のjs30%くらい

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,15 @@
 
 // ここから颯人
 $(function () {
-    $('.side_nav').hide();
+  $('.side_nav').hide();
 
-    $(window).scroll(function () { 
-        if ($(this).scrollTop()> 700) {
-            $('.side_nav').fadeIn();
-        }else{
-            $('.side_nav').fadeOut();
-        }
-    });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 700) {
+      $('.side_nav').fadeIn();
+    } else {
+      $('.side_nav').fadeOut();
+    }
+  });
 });
 
 
@@ -31,10 +31,44 @@ function showSlides() {
   let i;
   let slides = document.getElementsByClassName("hero_images");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 2000); 
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000);
 }
+
+
+
+
+
+
+
+// ここからこうよう
+const profile = [
+  {
+    id: 1,
+    name: '飯田紅葉',
+    POSSE: '③',
+    university: '慶應義塾',
+    hobby: ['サッカー', 'テニス', 'バスケ'],
+    addict: ['サッカー観戦', 'テニス観戦', 'バスケ観戦'],
+    message: '仮のテキストです',
+    interview: '仮のインタビューです',
+    image: 'koyo.jpg'
+  },
+  {
+    id: 2,
+    name: '三浦颯人',
+    POSSE: '③',
+    university: '明治',
+    hobby: ['サッカー', 'テニス', 'バスケ'],
+    addict: ['サッカー観戦', 'テニス観戦', 'バスケ観戦'],
+    message: '仮のテキストです',
+    interview: '仮のインタビューです',
+    image: 'hayato.jpg'
+  }
+]
+
+const statusBox = document.querySelectorAll('.status_box');
